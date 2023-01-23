1d1dd10fc26ed15782aee15751e1916125fbf497
omixjl <61581739+omixjl@users.noreply.github.com>
2023-01-23 23:33:37 +0100

    axios

--- app.js
+++ app.js
@@ -1,52 +1,18 @@
-const request = require('request');
-const express = require('express');
-const app = express();
+const axios = require('axios');
+const { access_token, page_id } = require('./config');
 
-
-const access_token = 'EAAMUxxxxxx';
-const page_id = '197xxxxxx'; 
-let message = 'Test 3';
-let image = 'https://pics.filmaffinity.com/Hello_World-314069290-large.jpg';
-
-
-const options = {
-    method: 'POST',
-    url: `https://graph.facebook.com/${page_id}/photos`,
-    qs: {
-        access_token: access_token,
-        message: message,
-        url: image
+const facebookPublish = async (message, url) => {
+    try {
+        await axios.post(`https://graph.facebook.com/${page_id}/photos`, {
+            access_token,
+            message,
+            url
+        });
+        console.log("Subido correctamente")
     }
-};
-
-request(options, function (error, response, body) {
-    if (error) {
+    catch (error) {
         console.log(error);
-    } else {
-        console.log(response.statusCode);
-        console.log(body);
     }
-});
+}
 
-
-/*
-app.post('/publish', (req, res) => {
-    const options = {
-        method: 'POST',
-        url: `https://graph.facebook.com/${page_id}/photos`,
-        qs: {
-            access_token: access_token,
-            message: message,
-            url: image
-        }
-    };
-
-   request(options, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(response.statusCode);
-        console.log(body);
-    }
-});
-*/
+facebookPublish('Test Api', 'https://pics.filmaffinity.com/Hello_World-314069290-large.jpg');
